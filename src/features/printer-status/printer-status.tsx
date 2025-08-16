1b4e1e53c52f44d15f46c447fc2120cf44f976df
Tomas Nasjleti <acatomas@gmail.com>
2025-08-16 13:22:31 +0200

    improvements

---
+++ src/features/printer-status/printer-status.tsx
@@ -0,0 +1,7 @@
+export default function PrinterStatus() {
+  return (
+    <div className="flex flex-col gap-2">
+      <h1>Printer Status</h1>
+    </div>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./app";
+import PrinterStatus from "./features/printer-status/printer-status";
+
 import { ThemeProvider } from "./providers/theme-provider";
+import { Toaster } from "@/components/ui/sonner";
 
 import "./index.css";
 
@@ -9,6 +12,8 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider defaultTheme="dark">
       <App />
+      <Toaster />
+      <PrinterStatus />
     </ThemeProvider>
   </React.StrictMode>
 );
