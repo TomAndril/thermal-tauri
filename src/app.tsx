a329bd5fa3ad0d0da4f370fbdf3b57ad2feee49d
Tomas Nasjleti <tomas.nasjleti@inter.ikea.com>
2025-08-20 17:04:34 +0200

    looking better

--- src/app.tsx
+++ src/app.tsx
@@ -2,7 +2,7 @@ import PrintText from "./features/print-text/print-text";
 
 export default function App() {
   return (
-    <main className="mx-auto max-w-md mt-18">
+    <main className="mx-auto max-w-lg mt-14">
       <PrintText />
     </main>
   );
